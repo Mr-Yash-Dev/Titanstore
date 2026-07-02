b43297befa4f2992846e04a8a347922ce24c8e64
Yash <titanmattersbot05@gmail.com>
2026-07-02 20:52:24 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,20 +1,8 @@
-# TitanXBots
-# File Path: plugins/__init__.py
-
 from aiohttp import web
-
 from .route import routes
 
-
 async def web_server() -> web.Application:
-    """
-    Creates and returns the aiohttp web application.
-    """
-
-    app = web.Application(
-        client_max_size=30 * 1024 * 1024  # 30 MB
-    )
-
+    app = web.Application(client_max_size=30000000)
     app.add_routes(routes)
-
     return app
+    
