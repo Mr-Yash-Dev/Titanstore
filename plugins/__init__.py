b4cc12dcf48f8c7c61cc2841d364c50004c1c9f2
Yash <titanmattersbot05@gmail.com>
2026-07-02 20:26:49 +0530

    Adjust client_max_size for aiohttp application
    Updated client_max_size to 30 MB in web application.

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,8 +1,20 @@
+# TitanXBots
+# File Path: plugins/__init__.py
+
 from aiohttp import web
+
 from .route import routes
 
+
 async def web_server() -> web.Application:
-    app = web.Application(client_max_size=30000000)
+    """
+    Creates and returns the aiohttp web application.
+    """
+
+    app = web.Application(
+        client_max_size=30 * 1024 * 1024  # 30 MB
+    )
+
     app.add_routes(routes)
+
     return app
-    
