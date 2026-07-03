2040003fba2873e08867661189e0a6a07e30d154
Yash <titanmattersbot05@gmail.com>
2026-07-03 10:24:57 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,29 +1,18 @@
 from datetime import datetime
 
-from pyrogram import filters
+from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from bot import Bot
-from config import BOT_STATS_TEXT, USER_REPLY_TEXT
+from config import BOT_STATS_TEXT
 from helper_func import get_readable_time
-from database.database import is_admin
 
-
-@Bot.on_message(filters.command("stats") & filters.private)
-async def stats(bot: Bot, message: Message):
-
-    if not await is_admin(message.from_user.id):
-        return
-
+@Client.on_message(filters.command("stats") & filters.private)
+async def stats(client: Client, message: Message):
+    # No admin check here anymore! Anyone can trigger this.
+    
     now = datetime.now()
-    delta = now - bot.uptime
+    delta = now - client.uptime
     uptime = get_readable_time(delta.seconds)
 
     await message.reply(BOT_STATS_TEXT.format(uptime=uptime))
-
-
-@Bot.on_message(filters.private & filters.incoming & ~filters.command(["stats"]))
-async def useless(_, message: Message):
-
-    if USER_REPLY_TEXT:
-        await message.reply(USER_REPLY_TEXT)
+    
