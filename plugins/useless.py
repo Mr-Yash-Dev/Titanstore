f40983852a4e13bf1c727ebb97f6a85eb390899a
Yash <titanmattersbot05@gmail.com>
2026-07-04 19:25:29 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from pyrogram import filters, Client
 from pyrogram.types import Message
@@ -10,7 +10,7 @@ from helper_func import get_readable_time
 async def stats(client: Client, message: Message):
     # No admin check here anymore! Anyone can trigger this.
     
-    now = datetime.now()
+    now = datetime.now(timezone.utc)
     delta = now - client.uptime
     uptime = get_readable_time(delta.seconds)
 
