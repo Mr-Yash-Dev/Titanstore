4af3bbb5d8c66c92779cbf11df19eb0fb76888c8
Yash <titanmattersbot05@gmail.com>
2026-07-04 18:53:47 +0530

    Fix import path for is_premium function

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyromod.exceptions import ListenerTimeout
 
 from helper_func import encode, get_message_id
-from database.Database import is_premium
+from database.database import is_premium
 
 @Client.on_message(filters.private & filters.command('batch'))
 async def batch(client: Client, message: Message):
