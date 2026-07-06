9c79804b7f4d1d7b1f7a321a173d998df0b9244c
Yash <titanmattersbot05@gmail.com>
2026-07-06 19:31:42 +0530

    TitanXBots ⚡

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ class Bot(Client):
             name="Bot",
             api_hash=API_HASH,
             api_id=APP_ID,
-            plugins={"root": "plugins"} if False else None, 
+            plugins={"root": "plugins"}, 
             workers=TG_BOT_WORKERS,
             bot_token=TG_BOT_TOKEN
         )
