29cebe78f07d6b2cb13df3b2f2397639ffc00976
Yash <titanmattersbot05@gmail.com>
2026-07-06 22:09:07 +0530

    TitanXBots ⚡

--- plugins/useless.py
+++ plugins/useless.py
@@ -4,9 +4,12 @@ from pyrogram.types import Message
 
 from config import BOT_STATS_TEXT
 from helper_func import get_readable_time
+from database.database import is_admin
 
 @Client.on_message(filters.command("stats") & filters.private)
 async def stats(client: Client, message: Message):
+    if not await is_admin(message.from_user.id): return await message.reply_text("⚠️ Access Denied: Admins only!")
+
     now = datetime.now(timezone.utc)
     delta = now - client.uptime
     uptime = get_readable_time(int(delta.total_seconds()))
