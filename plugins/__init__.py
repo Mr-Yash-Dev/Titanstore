6e3c1683cb5c23824c640ef8cf3a077a8d8bcc87
Yash <titanmattersbot05@gmail.com>
2026-07-06 22:15:52 +0530

    Delete plugins/__init__.py

--- plugins/__init__.py
+++
@@ -1,8 +0,0 @@
-from aiohttp import web
-from .route import routes
-
-async def web_server() -> web.Application:
-    app = web.Application(client_max_size=30000000)
-    app.add_routes(routes)
-    return app
-    
